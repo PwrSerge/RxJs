caacf140e4a471a6c3dca60f2e1e7cf9bee58239
Serge Guilmette <sergeguilmette@gmail.com>
2017-07-01 16:34:16 -0400

    RXJS tutorial first commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+const numbers = [33,22,55,66,77];
+const numbers$ = Rx.Observable.from(numbers);
+
+numbers$.subscribe(
+  v => {
+    console.log(v);
+  },
+  err => {
+    console.log(err);
+  },
+  complete => {
+    console.log('completed');
+  }
+);
